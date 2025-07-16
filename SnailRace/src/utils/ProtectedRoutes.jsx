eb80417a9d7b8a434db8c967d1891ed682dce68a
Gaston0009 <101906478+Gaston0009@users.noreply.github.com>
2025-07-16 00:04:38 -0300

    Update ProtectedRoutes.jsx

--- SnailRace/src/utils/ProtectedRoutes.jsx
+++ SnailRace/src/utils/ProtectedRoutes.jsx
@@ -3,12 +3,13 @@ import { AuthContext } from '../context/AuthContext';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 
   const ProtectedRoutes = ({ allowedRoles }) => {
-  const { user } = useContext(AuthContext); 
+  const { user, loading } = useContext(AuthContext); 
   const location = useLocation();
 
   const isAuthenticated = !!user;
   const userRole = user?.role;
 
+  if (loading) return <p>Cargando...</p>;
 
   if (!isAuthenticated) {
     return <Navigate to="/login" state={{ from: location }} replace />;
